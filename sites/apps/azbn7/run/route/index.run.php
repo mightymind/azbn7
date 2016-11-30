5a0ccee815f170f62bc05c24f8427b53cb6b8db4
Alexander <alexander.zybin@gmail.com>
2016-11-30 13:30:42 +0300

    201611301330

--- sites/apps/azbn7/run/route/index.run.php
+++ sites/apps/azbn7/run/route/index.run.php
@@ -5,5 +5,3 @@ $param['entity'] = $this->Azbn7->mdl('Entity')->item(1);
 $this->Azbn7->mdl('Site')
 	->render('entity/by_type/page', $param)
 ;
-
-echo $this->Azbn7->ch('<p class="default" >hello!</p>');
