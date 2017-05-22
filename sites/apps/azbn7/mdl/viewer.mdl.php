484bd513727b084e6653da096dcb4e1d535ee86f
azbn <i@azbn.ru>
2017-05-22 10:57:13 +0300

    201705221057

--- sites/apps/azbn7/mdl/viewer.mdl.php
+++ sites/apps/azbn7/mdl/viewer.mdl.php
@@ -272,4 +272,23 @@ class Viewer
 		return $this->wgt($matches[1], $p);
 	}
 	
+	public function echo_dev($a = array(), $for_all = false)
+	{
+		if($this->Azbn7->mdl('Site')->is('user') || $for_all) {
+			$bt = debug_backtrace();
+			$bt = $bt[0];
+			$dRoot = $_SERVER['DOCUMENT_ROOT'];
+			$dRoot = str_replace('/', "\\", $dRoot);
+			$bt['file'] = str_replace($dRoot, '', $bt['file']);
+			$dRoot = str_replace("\\", '/', $dRoot);
+			$bt['file'] = str_replace($dRoot, '', $bt['file']);
+			?>
+			<div style="background-color:#ffffff;color:#000000;outline:1px green solid;font-size:10px;" >
+				<div style="padding:5px 10px;background-color:green;color:#ffffff;font-weight:bold;" >File: <?=$bt['file'];?> [line: <?=$bt['line'];?>]</div>
+				<pre style="padding:10px;" ><? print_r($a);?></pre>
+			</div>
+			<?
+		}
+	}
+	
 }
---
+++ sites/apps/azbn7/run/route/page.run.php
@@ -0,0 +1,2 @@
+<?
+$this->Azbn7->mdl('Viewer')->echo_dev($param);
